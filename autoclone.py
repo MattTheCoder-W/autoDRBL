84567247159797b6958e7808178cbf0768bc4360
MattTheCoder-W <matigdynia@hotmail.com>
2021-12-14 18:01:07 +0100

    Updated files

--- autoclone.py
+++ autoclone.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-import os
-import sys
-import subprocess
-from curtsies.fmtfuncs import yellow, red, blue, green, bold
+import os, sys, subprocess
 from classes.drbltools import findIP
+from classes.interactive import *
 from bs4 import BeautifulSoup as bs
 import argparse
 
@@ -28,45 +26,6 @@ system image
 
 Author: Mateusz Wasaznik 3hSP
 """
-
-
-# Ta funkcja pozwala na proszenie uzytkownika w nieskonczonej petli do momentu popdania odpowiedzi spelniajacej kryteria
-# content - tresc zapytania, nospaces - czy nie zezwalac na spacje, numeric - czy odpowiedz ma byc liczba, inRange - zakres w jakim ma byc odpowiedz
-def secureInput(content, nospaces=False, numeric=False, inRange=None):
-    while True:
-        answ = input(content)
-        if not answ: # Jezeli brak odpowiedzi
-            print("Please specify value!")
-            continue
-        if nospaces:
-            if " " in answ: # jezeli spacja w odpowiedzi
-                print("No spaces allowed!")
-                continue
-        if numeric:
-            if not answ.isnumeric(): # jezeli odpowiedz nie jest liczba
-                print("This value has to be numeric!")
-                continue
-            answ = int(answ)
-        if inRange:
-            if not int(answ) in inRange: # jezeli odpowiedz jest poza zakresem
-                print("Number out of range!")
-                continue
-        break
-    return answ
-
-
-# Tekstowy wybor elementu z listy przez uzytkownika
-# lChoices - lista z wyborami, inputContent - tresc zapytania (jak content w secureInput())
-def choiceFromList(lChoices, inputContent=""):
-    for i, elem in enumerate(lChoices):
-        print(f"[{i}]", elem)
-
-    # wywolanie funkcji secureInput zwracajacej zweryfikowana odpowiedz,
-    # ktora ma nie miec spacji, ma byc liczba, oraz ma znajdowac sie w zakresie indeksow podanej listy
-    iChoice = secureInput(inputContent, nospaces=True, numeric=True, inRange=range(0, len(lChoices)))
-
-    return (iChoice, lChoices[iChoice])
-
 
 # Funkcja do wczytywania listy macow z pliku xml, wraz z wyborem sali oraz typu komputerow
 # sXMLFilePath - 
